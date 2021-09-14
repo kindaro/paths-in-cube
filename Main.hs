231c6b9854bfe01e47fbac77d6759e9f026c0cb9
Ignat Insarov <kindaro@gmail.com>
2021-09-14 12:34:52 +0500

    Hand-rolled memory trees are a disappointment.
    Performance is about 4 times worse than previously.

--- Main.hs
+++ Main.hs
@@ -26,7 +26,7 @@ import GHC.Exts qualified
 import GHC.TypeNats
 import Prelude
 import Prelude.Unicode
-import Relude (bimap)
+import Relude (bimap, Compose (..))
 import Relude.List qualified as List
 import Relude qualified
 import Generic.Data
@@ -98,10 +98,13 @@ instance KnownNat dimension ⇒ Memoizable (Vector dimension Int) where
         where
           polynomial vector = Vector.zipWith (\ digit power → digit * get @SizeOfTheBox reifiedNat^power) vector (enumFrom0 @Int)
 
-instance {-# overlapping #-} KnownNat dimension ⇒ Memoizable [Vector dimension Int] where
-  memoize function = \ input → fromMaybe (function input) (findInTree memory input)
+instance {-# overlapping #-}
+  ( KnownNat dimension
+  , HierarchicalMemory [Vector dimension Int] (Cofree (Tree dimension))
+  ) ⇒ Memoizable [Vector dimension Int] where
+  memoize function = findInHierarchicalTree memory
     where
-      memory = GHC.Exts.lazy (buildTree (Set.fromList theBox)) function
+      memory = GHC.Exts.lazy buildHierarchicalTree function
 
 data Pair α = Pair α α deriving (Show, Eq, Ord, Functor, Foldable, Traversable, Generic, Generic1)
 
@@ -128,6 +131,7 @@ buildWordTree function = Recursion.unfold unfolding 1
         branch = Pair (seed * 2) (seed * 2 + 1)
 
 findInWordTree ∷ Cofree Pair α → Relude.Word → α
+findInWordTree _ 0 = error "Only positive indices are accepted, 0 given — cannot answer!"
 findInWordTree cofree index = worker cofree (numberOfBinaryDigitsOf index - 1) (index - 2^(numberOfBinaryDigitsOf index - 1))
   where
     worker (extract → value) 0 0 = value
@@ -146,62 +150,79 @@ findInIntTree (valueAt0 :< Pair treeOfNegativeValues treeOfPositiveValues) key =
   LT → findInWordTree treeOfNegativeValues (negate (fromIntegral key))
   EQ → valueAt0
   GT → findInWordTree treeOfPositiveValues (fromIntegral key)
+
 numberOfBinaryDigitsOf ∷ Word → Relude.Natural
 numberOfBinaryDigitsOf = floor ∘ (+1) ∘ logBase @Float 2 ∘ fromIntegral
-
-decompose ∷ Word → [Bool]
-decompose 0 = [ ]
-decompose number =
-  let mostSignificantDigit = numberOfBinaryDigitsOf number
-  in List.unfoldr unfolding (number, mostSignificantDigit)
-  where
-    unfolding (_, 0) = Nothing
-    unfolding (number, mostSignificantDigit) = Just if number ≥ 2^(mostSignificantDigit - 1)
-      then (True, (number - 2^(mostSignificantDigit - 1), mostSignificantDigit - 1))
-      else (False, (number, mostSignificantDigit - 1))
-
-buildTree ∷ ∀ key value. Set key → ([key] → value) → Cofree (Map key) value
-buildTree keySet function = Recursion.unfold unfolding [ ]
-  where
-    unfolding ∷ [key] → CofreeTransformer.CofreeF (Map key) value [key]
-    unfolding keys = function keys CofreeTransformer.:< Map.fromSet (: keys) keySet
-
-findInTree ∷ Ord key ⇒ Cofree (Map key) value → [key] → Maybe value
-findInTree (extract → value) [ ] = Just value
-findInTree (Cofree.unwrap → branch) (key: keys) = do
-  subCofree ← Map.lookup key branch
-  findInTree subCofree keys
 
 data Tree dimension α where
   Leaf ∷ Cofree Pair α → Tree 1 α
   Branch ∷ Cofree Pair (Tree (dimension - 1) α) → Tree dimension α
 
-class HierarchicalMemory dimension where
-  buildHierarchicalTree ∷ (Vector dimension Int → α) → Tree dimension α
-  findInHierarchicalTree ∷ Tree dimension α → (Vector dimension Int → α)
+instance {-# overlapping #-} Functor (Tree 1) where
+  fmap function (Leaf cofree) = Leaf (fmap function cofree)
+  fmap _ (Branch _) = error "Impossible: there are only leaves of dimension 1."
 
-  memoizeWithHierarchicalTree ∷ (Vector dimension Int → α) → Vector dimension Int → α
+instance (Functor (Tree (dimension - 1))) ⇒ Functor (Tree dimension) where
+  fmap function (Branch cofree) = Branch ((fmap ∘ fmap) function cofree)
+  fmap _ (Leaf _) = error "Impossible: branches are always of dimension at least 2."
+
+class HierarchicalMemory input memory | input → memory, memory → input where
+  buildHierarchicalTree ∷ (input → α) → memory α
+  findInHierarchicalTree ∷ memory α → input → α
+
+  memoizeWithHierarchicalTree ∷ (input → α) → input → α
   memoizeWithHierarchicalTree = findInHierarchicalTree ∘ buildHierarchicalTree
 
-instance {-# overlapping #-} HierarchicalMemory 1 where
-  buildHierarchicalTree = Leaf ∘ buildIntTree ∘ (∘ Vector.singleton)
-  findInHierarchicalTree (Leaf cofree) = findInIntTree cofree ∘ Vector.head
+instance HierarchicalMemory Int (Cofree Pair) where
+  buildHierarchicalTree = buildIntTree
+  findInHierarchicalTree = findInIntTree
+
+instance {-# overlapping #-} HierarchicalMemory (Vector 1 Int) (Tree 1) where
+  buildHierarchicalTree = Leaf ∘ buildHierarchicalTree ∘ (∘ Vector.singleton)
+  findInHierarchicalTree (Leaf cofree) = findInHierarchicalTree cofree ∘ Vector.head
   findInHierarchicalTree (Branch _) = error "Impossible: branches are always of dimension at least 2."
 
 instance
-  ( HierarchicalMemory previousDimension
+  ( HierarchicalMemory (Vector previousDimension Int) (Tree previousDimension)
   , dimension ~ (1 + previousDimension)
-  , dimension ~ (previousDimension + 1)
   , previousDimension ~ (dimension - 1)
-  ) ⇒ HierarchicalMemory dimension where
+  ) ⇒ HierarchicalMemory (Vector dimension Int) (Tree dimension) where
   buildHierarchicalTree ∷ ∀ α. (Vector dimension Int → α) → Tree dimension α
   buildHierarchicalTree function = Branch (buildIntTree curriedFunction)
     where
       curriedFunction ∷ Int → Tree (dimension - 1) α
       curriedFunction number = buildHierarchicalTree \ vector → function (number `Vector.cons` vector)
+
   findInHierarchicalTree ∷ Tree dimension α → (Vector dimension Int → α)
   findInHierarchicalTree (Branch cofree) vector = findInHierarchicalTree (findInIntTree cofree (Vector.head vector)) (Vector.tail vector)
   findInHierarchicalTree (Leaf _) _ = error "Impossible: there are only leaves of dimension 1."
+
+type tube ∘ pipe = Compose tube pipe
+
+instance HierarchicalMemory [Int] (Cofree (Cofree Pair)) where
+  buildHierarchicalTree function = Recursion.unfold unfolding id
+    where
+      unfolding differenceListOfKeys = fruit CofreeTransformer.:< branches
+        where
+          fruit = function (differenceListOfKeys [ ])
+          branches = buildHierarchicalTree \ key → differenceListOfKeys ∘ (key:)
+
+  findInHierarchicalTree (fruit :< _) [ ] = fruit
+  findInHierarchicalTree (_ :< branches) (key: keys) = findInHierarchicalTree (findInHierarchicalTree branches key) keys
+
+instance
+  ( HierarchicalMemory (Vector dimension Int) (Tree dimension)
+  , Functor (Tree dimension)
+  ) ⇒ HierarchicalMemory [Vector dimension Int] (Cofree (Tree dimension)) where
+  buildHierarchicalTree function = Recursion.unfold unfolding id
+    where
+      unfolding differenceListOfKeys = fruit CofreeTransformer.:< branches
+        where
+          fruit = function (differenceListOfKeys [ ])
+          branches = buildHierarchicalTree \ key → differenceListOfKeys ∘ (key:)
+
+  findInHierarchicalTree (fruit :< _) [ ] = fruit
+  findInHierarchicalTree (_ :< branches) (key: keys) = findInHierarchicalTree (findInHierarchicalTree branches key) keys
 
 enumFrom0 ∷ ∀ number length. (Num number, Enum number, Vector.Unbox number, KnownNat length) ⇒ Vector length number
 enumFrom0 = Vector.enumFromN 0
@@ -257,7 +278,7 @@ unfolding walk@Walk {..} = case step walk of
   [ ] → if Set.size visitedVectors ≡ sizeOfTheBox ^ get @dimension @Int reifiedNat then Left walk else Right [ ]
   possibleNextWalks → Right possibleNextWalks
 
-grow ∷ KnownNat dimension ⇒ WalkUpToSymmetry dimension → Set (Either (WalkUpToSymmetry dimension) (WalkUpToSymmetry dimension))
+grow ∷ (KnownNat dimension, HierarchicalMemory [Vector dimension Int] (Cofree (Tree dimension))) ⇒ WalkUpToSymmetry dimension → Set (Either (WalkUpToSymmetry dimension) (WalkUpToSymmetry dimension))
 grow = either (Set.singleton ∘ Left ∘ WalkUpToSymmetry) (Set.map (Right ∘ WalkUpToSymmetry) ∘ Set.fromList) ∘ unfolding ∘ walkUpToSymmetry
 
 iterator
@@ -349,15 +370,15 @@ canonicalize
 canonicalize symmetries = minimum ∘ orbit symmetries
 
 representativePath
-  ∷ ∀ dimension. KnownNat dimension
+  ∷ ∀ dimension. (KnownNat dimension, HierarchicalMemory [Vector dimension Int] (Cofree (Tree dimension)))
   ⇒ WalkUpToSymmetry dimension → (Vector dimension Int, [Vector dimension Int])
 representativePath (WalkUpToSymmetry Walk {..}) = (currentPosition', path')
   where
     path' = memoize (canonicalize (allHyperoctahedralGroup @dimension)) path
     currentPosition' = memoize (canonicalize (allHyperoctahedralGroup @dimension)) currentPosition
 
-instance KnownNat dimension ⇒ Eq (WalkUpToSymmetry dimension) where
+instance (KnownNat dimension, HierarchicalMemory [Vector dimension Int] (Cofree (Tree dimension))) ⇒ Eq (WalkUpToSymmetry dimension) where
   (==) = (≡) `on` representativePath
 
-instance KnownNat dimension ⇒ Ord (WalkUpToSymmetry dimension) where
+instance (KnownNat dimension, HierarchicalMemory [Vector dimension Int] (Cofree (Tree dimension))) ⇒ Ord (WalkUpToSymmetry dimension) where
   compare = compare `on` representativePath
